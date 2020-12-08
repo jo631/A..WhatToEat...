02af057922f0dddc588d3e0ad5620f6cb29d7f8b
jo631 <52406729+jo631@users.noreply.github.com>
2020-12-09 02:43:13 +0900

    Revert "jdbc 연결 성공"
    This reverts commit 2bedccb039537bbc0980592b83ba485797d9dd37.

--- src/dao/JDBC.java
+++ src/dao/JDBC.java
@@ -4,24 +4,31 @@ import java.sql.*;
 
 public class JDBC {
 	Connection conn = null;
-	Statement stmt = null;
-	final String url = "jdbc:mysql://localhost:3306/WhatToEat";
-	final String userName = "root";
-	final String pw = "database";
-
+	String url = "jdbc:mysql://localhost/whattoeat";
+	String userName = "root";
+	String pw = "database";
+	
 	public JDBC() {
-		try {
+		try
+		{
 			Class.forName("com.mysql.jdbc.Driver");
+			// @param getConnection(url, userName, password);
+			// @return Connection
 			conn = DriverManager.getConnection(url, userName, pw);
 			System.out.println("연결 성공");
 		}
-		catch (ClassNotFoundException e) {
+		
+		catch(ClassNotFoundException e)
+		{
 			System.out.println("드라이버 로딩 실패");
-		} catch (SQLException e) {
+		}
+		catch(SQLException e)
+		{
 			System.out.println("에러: " + e);
 		}
-
-		finally {
+		
+		finally
+		{
 			try {
 				if (conn != null && !conn.isClosed()) {
 					conn.close();
@@ -30,7 +37,6 @@ public class JDBC {
 				e.printStackTrace();
 			}
 		}
-		
 	}
-
+	
 }
